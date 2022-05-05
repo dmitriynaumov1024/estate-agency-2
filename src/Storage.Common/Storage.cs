1c4dda53d30ecdc944af5e755eb1ed8667e703e9
dmitriynaumov1024 <naumov1024@gmail.com>
2022-05-05 10:29:24 +0300

    Storage.Common: add Value-only storage (like SortedSet)

--- src/Storage.Common/Storage.cs
+++ src/Storage.Common/Storage.cs
@@ -4,14 +4,26 @@ using System.Collections.Generic;
 namespace Storage.Common
 {
     public abstract class Storage<TKey, TValue> 
+    where TKey: IComparable<TKey>
     where TValue: class
     {
         public abstract TValue Get (TKey key);
-        public abstract IEnumerable<TValue> Filter (FilterInfo filter, int? limit = null);
+        public abstract IEnumerable<KeyValuePair<TKey, TValue>> Filter (FilterInfo filter, int? limit = null);
         public abstract TKey Put (TValue value);
         public abstract TValue Replace (TKey key, TValue newValue);
         public abstract TValue Delete (TKey key);
         public abstract IEnumerable<KeyValuePair<TKey, TValue>> AsEnumerable();
+        public abstract bool Clear();
+    }
+
+    public abstract class Storage<TValue> 
+    where TValue: class, IComparable<TValue>
+    {
+        public abstract void Put(TValue value);
+        public abstract IEnumerable<TValue> Filter (FilterInfo filter, int? limit = null);
+        public abstract bool Contains(TValue value);
+        public abstract TValue Delete(TValue value);
+        public abstract IEnumerable<TValue> AsEnumerable();
         public abstract bool Clear();
     }
 }
